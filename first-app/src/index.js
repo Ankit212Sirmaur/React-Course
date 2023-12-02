e7283d7c565767876eadcc1abd135f21d1cc5512
AnkitKumar <ankit17110747@yahoo.com>
2023-12-02 20:53:23 +0530

    it render only which element have to render

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const heading = <h1>heading 1</h1>
@@ -10,16 +10,36 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   <h2>heading 2</h2>
 // </div>
 
-const a = 5+2;
-const container = (
-  <div>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, quidem?</p>
-    <h2>heading 2 is {a}</h2>
-  </div>
-)
+// const heading = {
+//   backgroundColor: "green",
+//   border: '2px solid red',
+//   // justify-content: 'center'
+//   textAlign: 'center'
+// }
 
-root.render(
-  container
-);
+// const a = 5+2;
+// const container = (
+//   <div>
+//     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, quidem?</p>
+//     <h2 style={heading}>heading 2 is {a}</h2>
+//     <p className='para'>Lorem ipsum dolor sit amet.</p>
+//   </div>
+// )
+
+function myclock() {
+  root.render(
+    <div>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <h2>heading 2</h2>
+    </div>
+  );
+  setTimeout(myclock, 1000);
+}
+
+myclock();
+
+// root.render(
+//   myclock
+// );
 
 
